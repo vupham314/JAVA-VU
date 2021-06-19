e79e9c24fe16bf53515f5e1108e09d2460cdfd8f
vupham314 <work.vupham@gmail.com>
2021-06-19 21:26:23 +0700

    fdas

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("good");
         System.out.print("sdf");
+        System.out.println("update");
     }
 }
